b25fe8575d0248693c21d70f7456e57460659f30
Sarah Bindman <sbindman@spatialdev.com>
2015-06-15 09:58:31 -0700

    dropdown functions as it should and displays appropriate data

--- app.js
+++ app.js
@@ -1,35 +1,52 @@
 // Different File ===================================
-var fileUpload = function(number) {
+var fileUpload = function() {
 
-    this.numColumns = number;
+    this.numColumns = 5;
     this.numRows = 200;
-    this.latColumn = number % 3;
+    this.latColumn = 2;
     this.nameColumn = 1;
     this.lngColumn = null;
-    this.headers = ['id_number', 'location', 'latitude'];
+    this.headers = ['id_number', 'location', 'latitude', 'lonGitude', 'wellness'];
+
+    this.latName = function () {
+        if (this.latColumn != null) {
+            return this.headers[this.latColumn]
+        }
+    };
+
+    this.longName = function () {
+        if (this.lngColumn != null) {
+            return this.headers[this.lngColumn]
+        }
+    };
+
+    this.nameName = function () {
+        if (this.nameColumn != null) {
+            return this.headers[this.nameColumn]
+        }
+    };
 }
 
-var file1 = new fileUpload(5);
-var file2 = new fileUpload(8);
+
+var file1 = new fileUpload();
+
 
 //======================================================
 
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', [])
+//    .config(['$routeProvider', function($routeProvider) {
+//    $routeProvider.when('/upload', {templateUrl: 'partials/uploaded.html', controller: 'MyCtrl'});
+//    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+//    $routeProvider.otherwise({redirectTo: '/view1'});
+//}]);
 
 myApp.controller('MyCtrl', function($scope) {
-
-    $scope.address =
-        {'state': ['AL', 'MA']}
-    ;
-
-    $scope.lov_state = [
-        {'lookupCode': 'AL', 'description': 'Alabama'},
-        {'lookupCode': 'FL', 'description': 'Florida'},
-        {'lookupCode': 'CA', 'description': 'California'},
-        {'lookupCode': 'DE', 'description': 'Delaware'}
-    ];
-
     $scope.uploadedFile = file1;
+
+    $scope.latName = file1.latName();
+    $scope.nameName = file1.nameName();
+    $scope.longName = file1.longName();
+
 });
 
 
